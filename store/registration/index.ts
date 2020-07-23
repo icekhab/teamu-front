999c4efdec95e3e2954be881cf650d52daf1aeec
Evgeniy Logvinov <evgeniy.logvinov@cyoda.com>
2020-07-23 19:51:27 +0300

    Добавил стор который потерял при пул реквесте

---
+++ store/registration/index.ts
@@ -0,0 +1,36 @@
+import { ActionContext } from 'vuex';
+import RegistrationState from '@/entities/states/RegistrationState';
+import RootState from '@/entities/states/RootState';
+import RegistrationService from '@/services/RegistrationService';
+import SignUpEntity from '@/entities/SignUpEntity';
+import AuthInfoEntity from '@/entities/AuthInfoEntity';
+
+export const state = (): RegistrationState => ({
+  signup: {
+    email: '',
+    name: '',
+    openLandProfileLink: '',
+    password: '',
+    userRole: '',
+    verifyPassword: '',
+  },
+  authInfo: {
+    expiresAt: '',
+    string: '',
+    userId: 0,
+  },
+});
+
+export const actions : any = {
+  async postSignUp(context: ActionContext<RegistrationState, RootState>, signup: SignUpEntity) {
+    const authInfo: AuthInfoEntity = await RegistrationService.postSignUp(signup);
+
+    context.commit('setSignUpInfo', authInfo);
+  },
+};
+
+export const mutations = {
+  setSignUpInfo(currentState: RegistrationState, authInfo: AuthInfoEntity): void {
+    currentState.authInfo = authInfo;
+  },
+};
