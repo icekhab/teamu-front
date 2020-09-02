eb4ad50522752eba4034977a7147e1c40181f4a4
Evgeniy Logvinov <evgeniy.logvinov@cyoda.com>
2020-09-02 22:14:17 +0300

    Пробую статические значения

--- components/project/ProjectInfo.vue
+++ components/project/ProjectInfo.vue
@@ -89,13 +89,14 @@ const namespace = 'project';
     TButton,
     Avatar,
   },
-  head(this: ProjectInfoComponent): object {
+  head(): object {
+  // head(this: ProjectInfoComponent): object {
     return {
       title: 'Test title for card',
       meta: [
         { hid: 'og:type', property: 'og:type', content: 'article' },
-        { hid: 'og:image', property: 'og:image', content: this.projectImg },
-        { hid: 'og:title', property: 'og:title', content: this.project.name },
+        { hid: 'og:image', property: 'og:image', content: 'https://i.imgur.com/OYrHoWa.png' },
+        // { hid: 'og:title', property: 'og:title', content: this.project.name },
       ],
     };
   },
