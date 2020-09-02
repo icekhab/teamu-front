cd98bb6998998b70b29cc67544599fca19546b7d
Evgeniy Logvinov <evgeniy.logvinov@cyoda.com>
2020-09-02 13:37:35 +0300

    Проверяю на henerator hooks

--- config/hooks.js
+++ config/hooks.js
@@ -6,4 +6,7 @@ module.exports = {
       app.use(writeAccessLog);
     },
   },
+  'generate:page': page => {
+    page.html = page.html.replace(/ data-n-head=".*?"/gi, '').replace(/ data-hid=".*?"/gi, '')
+  },
 };
